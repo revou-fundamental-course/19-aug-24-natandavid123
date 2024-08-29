d3705a7328b51a216079986f317a867e2aed5d50
natandavid123 <david.wibowo25@gmail.com>
2024-08-29 12:43:24 +0700

    update style

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-function forValidation(){
-    let name = document.getElementById('name').value;
-    if(name ==''){
-        alert('formnya kosong');
-    } else{
-        alert('form berhasil disubmit');
-    }
-    console.log(name);
-}
 
 let indexSlide = 1;
 showBanner(1);
@@ -35,17 +26,74 @@ function showBanner(indexBanner){
 
 
 function validateForm() {
-    const name = document.forms['message-form']['full-name'].value;
-    const birthDate = document.forms['message-form']['birth-date'].value;
+    const name = document.forms['message-form']['name'].value;
+    const birthDate = document.forms['message-form']['birthdate'].value;
     const gender = document.forms['message-form']['gender'].value;
-    const messages = document.forms['message-form']['messages'].value;
+    const messages = document.forms['message-form']['message'].value;
   
-    if (name == '' || birthDate == '' || gender == '' || messages == '') {
-      alert('Tidak boleh kosong');
-      return false;
+    if(name == '' && birthDate == '' && gender == '' && messages == ''){
+        alert('Form tidak boleh kosong');
+        return false;
     }
+    if (name == '') {
+      alert('Nama tidak boleh kosong');
+      return false;
+    } else if(birthDate == ''){
+        alert('Birth Date tidak boleh kosong');
+        return false;
+    } else if(gender == ''){
+        alert('Gender tidak boleh kosong');
+        return false;
+    } else if(messages == ''){
+        alert('Message tidak boleh kosong');
+        return false;
+    } else{
+        alert('Form berhasil disubmit! Arigatokozaimas!!')
+        return false;
+        
+    }
+
+    
+    nameInformation.innerHTML = "<span>Nama anda adalah<span/>" + name;
+    
   
     setSenderUI(name, birthDate, gender, messages);
   
     return false;
+
+
+    
 }
+
+
+function showInformation(){
+
+    const name1 = document.getElementByID('name');
+    const birthdate1 = document.getelementbyID("birthdate");
+    const gender1 = document.getelementbyID("male") || document.getelementbyID("female");
+    const message1 = document.getelementbyID("message");
+    const nameInformation = document.getelementbyID("name-information");
+    const birthdateInformation = document.getelementbyID("birthdate");
+    const genderInformation = document.getElementsByClassName("radio-group");
+    const messageInformation = document.getelementbyID("message");
+
+    function fun1(){
+        nameInformation.innerHTML = name1.value;
+        birthdateInformation.innerHTML = birthdate1.value;
+        genderInformation.innerHTML = gender1.value;
+        messageInformation.innerHTML = message1.value;
+    }
+
+}
+
+   
+
+
+// function informationForm(){
+//     var nameinformation = document.getElementById("name-information");
+//     var birthDateInformation = document.getElementById("birthdate-information");
+//     var genderInformation = document.getElementByClass("radio-group-information");
+//     var messagesInformation = document.getElementById("message-information");
+
+//     nameInformation.innerHTML = "<span>Nama anda adalah<span/>" + name;
+// }
